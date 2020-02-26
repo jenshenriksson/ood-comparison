94566db7c23d88677319e59abc8040e4492a27c6
Jens Henriksson <Jens.Henriksson@semcon.com>
2020-02-26 15:18:22 +0100

    Script for running all experiemnts

--- scripts/run_experiments.sh
+++ scripts/run_experiments.sh
@@ -4,3 +4,4 @@ python3 run_saved_epochs.py --model DenseNet_augm --supervisor $1 --outliers $2 
 python3 run_saved_epochs.py --model VGG_augm --supervisor $1 --outliers $2 --subset $3
 python3 run_saved_epochs.py --model WRN28_augm --supervisor $1 --outliers $2 --subset $3
 python3 run_saved_epochs.py --model WRN40_augm --supervisor $1 --outliers $2 --subset $3
+
